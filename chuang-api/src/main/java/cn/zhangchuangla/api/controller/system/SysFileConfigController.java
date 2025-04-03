83273a0b64f8e8e47464f790301a6ccb0ac68b99
zhangchuang <chuang003@outlook.com>
2025-04-03 22:37:54 +0800

    refactor(system): 重构文件配置模块
    - 删除了 FileConfigController 类
    - 重命名 SysFileConfigController 为 FileConfigController- 移除了不必要的导入和注解
    - 优化了类结构和方法命名

--- chuang-api/src/main/java/cn/zhangchuangla/api/controller/system/SysFileConfigController.java
+++ chuang-api/src/main/java/cn/zhangchuangla/api/controller/system/SysFileConfigController.java
@@ -1,179 +1,71 @@
 package cn.zhangchuangla.api.controller.system;
 
-import cn.zhangchuangla.common.annotation.Log;
-import cn.zhangchuangla.common.constant.RedisKeyConstant;
-import cn.zhangchuangla.common.constant.SystemMessageConstant;
 import cn.zhangchuangla.common.core.controller.BaseController;
-import cn.zhangchuangla.common.core.redis.ConfigCacheService;
-import cn.zhangchuangla.common.core.redis.RedisCache;
-import cn.zhangchuangla.common.entity.file.AliyunOSSConfigEntity;
-import cn.zhangchuangla.common.entity.file.LocalFileConfigEntity;
-import cn.zhangchuangla.common.entity.file.MinioConfigEntity;
-import cn.zhangchuangla.common.enums.BusinessType;
-import cn.zhangchuangla.common.enums.DefaultFileUploadEnum;
+import cn.zhangchuangla.common.core.page.TableDataResult;
 import cn.zhangchuangla.common.result.AjaxResult;
-import cn.zhangchuangla.common.utils.PathUtils;
-import cn.zhangchuangla.system.model.request.file.DefaultFileConfigRequest;
+import cn.zhangchuangla.system.model.entity.SysFileConfig;
+import cn.zhangchuangla.system.model.request.file.SysFileConfigAddRequest;
+import cn.zhangchuangla.system.model.request.file.SysFileConfigListRequest;
+import cn.zhangchuangla.system.model.vo.file.manage.FileManagementListVo;
+import cn.zhangchuangla.system.service.SysFileConfigService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
- * 文件管理控制器
- * <p>
- * created on 2025/3/20 16:43
+ * @author zhangchuang
+ * Created on 2025/4/3 21:39
  */
-@Slf4j
+
 @RestController
 @RequestMapping("/system/file/config")
 @Tag(name = "文件配置")
 public class SysFileConfigController extends BaseController {
 
-    private final RedisCache redisCache;
-    private final ConfigCacheService configCacheService;
+
+    private final SysFileConfigService sysFileConfigService;
+
 
     @Autowired
-    public SysFileConfigController(RedisCache redisCache, ConfigCacheService configCacheService) {
-        this.redisCache = redisCache;
-        this.configCacheService = configCacheService;
+    public SysFileConfigController(SysFileConfigService sysFileConfigService) {
+        this.sysFileConfigService = sysFileConfigService;
     }
 
+
     /**
-     * 更新minio配置
+     * 文件配置列表
      *
-     * @param request 请求参数
-     * @return 操作结果
+     * @param request 查询参数
+     * @return 文件配置列表
      */
-    @PutMapping("/config/minio")
-    @Log(title = "文件管理", businessType = BusinessType.UPDATE)
-    @Operation(summary = "更新Minio配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult updateMinioConfig(@RequestBody @Validated MinioConfigEntity request) {
-        redisCache.setCacheObject(RedisKeyConstant.SYSTEM_FILE_UPLOAD_SERVICE_SELECT_MINIO, request);
-        configCacheService.refreshAllConfigs();
-        return success(SystemMessageConstant.UPDATE_SUCCESS);
+    @Operation(summary = "文件配置列表", description = "文件配置列表")
+    @GetMapping("/list")
+    @PreAuthorize("@auth.hasAnyPermission('system:file-config:list')")
+    public TableDataResult listSysFileConfig(SysFileConfigListRequest request) {
+        Page<SysFileConfig> sysFileConfigPage = sysFileConfigService.listSysFileConfig(request);
+        List<FileManagementListVo> fileManagementListVos = copyListProperties(sysFileConfigPage, FileManagementListVo.class);
+        return getTableData(sysFileConfigPage, fileManagementListVos);
     }
 
+
     /**
-     * 更新阿里云OSS配置
+     * 新增文件配置
      *
-     * @param request 请求参数
-     * @return 操作结果
+     * @param request 文件配置信息
+     * @return 新增结果
      */
-    @PutMapping("/config/oss")
-    @Log(title = "文件管理", businessType = BusinessType.UPDATE)
-    @Operation(summary = "更新阿里云OSS配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult updateAliyunOSSConfig(@RequestBody @Validated AliyunOSSConfigEntity request) {
-        redisCache.setCacheObject(RedisKeyConstant.SYSTEM_FILE_UPLOAD_SERVICE_SELECT_ALIYUN, request);
-        configCacheService.refreshAllConfigs();
-        return success(SystemMessageConstant.UPDATE_SUCCESS);
-    }
-
-    /**
-     * 更新本地文件上传配置
-     *
-     * @param request 请求参数
-     * @return 操作结果
-     */
-    @PutMapping("/config/local")
-    @Log(title = "文件管理", businessType = BusinessType.UPDATE)
-    @Operation(summary = "更新本地文件上传配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult updateLocalFileConfig(@RequestBody LocalFileConfigEntity request) {
-        String uploadPath = request.getUploadPath();
-        // 1. 校验路径
-        if (PathUtils.isWindowsPath(uploadPath)) {
-            //  2. 处理 Windows 路径
-            uploadPath = PathUtils.processWindowsPath(uploadPath);
-            request.setUploadPath(uploadPath);
-        } else {
-            return AjaxResult.error("路径格式不正确，请检查！");
-        }
-        // 3. 保存到 Redis
-        redisCache.setCacheObject(RedisKeyConstant.SYSTEM_FILE_UPLOAD_SERVICE_SELECT_LOCAL, request);
-        configCacheService.refreshAllConfigs();
-        return AjaxResult.success(SystemMessageConstant.UPDATE_SUCCESS);
-    }
-
-    /**
-     * 更新默认文件上传配置
-     *
-     * @param request 请求参数
-     * @return 操作结果
-     */
-    @PutMapping("/config/default")
-    @Log(title = "文件管理", businessType = BusinessType.UPDATE)
-    @Operation(summary = "更新默认文件上传配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult updateDefaultFileConfig(@RequestBody DefaultFileConfigRequest request) {
-        DefaultFileUploadEnum defaultFileUploadEnum = DefaultFileUploadEnum.getByName(request.getFileUploadType());
-        if (defaultFileUploadEnum == null) {
-            error("您的输入错误，请检查后再输入!");
-        }
-        // 存储枚举的name值，比如"local", "minio", "oss"
-        if (defaultFileUploadEnum != null) {
-            redisCache.setCacheObject(RedisKeyConstant.SYSTEM_FILE_UPLOAD_SERVICE_SELECT_DEFAULT, defaultFileUploadEnum.getName());
-            configCacheService.refreshAllConfigs();
-            return success(SystemMessageConstant.UPDATE_SUCCESS);
-        }
-        return error(SystemMessageConstant.UPDATE_FAIL);
-    }
-
-    /**
-     * 刷新配置缓存
-     */
-    @PostMapping("/config/refresh")
-    @Log(title = "文件管理", businessType = BusinessType.REFRESH)
-    @Operation(summary = "刷新文件配置缓存", description = "当修改文件配置时无法正常指定选择的配置，可以先执行次接口尝试重新刷新尝试")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult refreshConfig() {
-        // 现在ProfileException会被正常抛出并由全局异常处理器捕获
-        configCacheService.refreshAllConfigs();
-        return success("配置刷新成功");
-    }
-
-
-    /**
-     * 获取本地文件上传配置
-     *
-     * @return 本地文件上传配置
-     */
-    @GetMapping("/config/local")
-    @Operation(summary = "获取本地文件上传配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult getLocalFileConfig() {
-        LocalFileConfigEntity localFileConfig = configCacheService.getLocalFileConfig();
-        return AjaxResult.success(localFileConfig);
-    }
-
-    /**
-     * 获取Minio文件上传配置
-     *
-     * @return Minio文件上传配置
-     */
-    @GetMapping("/config/minio")
-    @Operation(summary = "获取Minio文件上传配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult getMinioConfig() {
-        MinioConfigEntity minioConfig = configCacheService.getMinioConfig();
-        return AjaxResult.success(minioConfig);
-    }
-
-    /**
-     * 获取阿里云OSS文件上传配置
-     *
-     * @return 阿里云OSS文件上传配置
-     */
-    @GetMapping("/config/oss")
-    @Operation(summary = "获取阿里云OSS文件上传配置")
-    @PreAuthorize("@auth.hasPermission('system:file:config')")
-    public AjaxResult getAliyunOSSConfig() {
-        AliyunOSSConfigEntity aliyunOSSConfig = configCacheService.getAliyunOSSConfig();
-        return AjaxResult.success(aliyunOSSConfig);
+    @Operation(summary = "新增文件配置", description = "新增文件配置")
+    @PreAuthorize("@auth.hasAnyPermission('system:file-config:add')")
+    @GetMapping("/add")
+    public AjaxResult saveFileConfig(SysFileConfigAddRequest request) {
+        boolean result = sysFileConfigService.saveFileConfig(request);
+        return toAjax(result);
     }
 }
