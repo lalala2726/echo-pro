98c862e8a5bf8f9aa0dea5f1cf2f21e44914c862
Chuang <chuang003@outlook.com>
2025-05-26 15:37:59 +0800

    feat(message): 添加用户消息已读表及相关代码
    - 新增 UserMessageRead 实体类
    - 创建 UserMessageReadMapper 接口及映射文件
    - 添加 UserMessageReadService 接口及实现类

---
+++ chuang-system/chuang-system-message/src/main/java/cn/zhangchuangla/message/service/UserMessageReadService.java
@@ -0,0 +1,11 @@
+package cn.zhangchuangla.message.service;
+
+import cn.zhangchuangla.message.model.entity.UserMessageRead;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author Chuang
+ */
+public interface UserMessageReadService extends IService<UserMessageRead> {
+
+}
---
+++ chuang-system/chuang-system-message/src/main/java/cn/zhangchuangla/message/service/impl/UserMessageReadServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.zhangchuangla.message.service.impl;
+
+import cn.zhangchuangla.message.mapper.UserMessageReadMapper;
+import cn.zhangchuangla.message.model.entity.UserMessageRead;
+import cn.zhangchuangla.message.service.UserMessageReadService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Chuang
+ */
+@Service
+public class UserMessageReadServiceImpl extends ServiceImpl<UserMessageReadMapper, UserMessageRead>
+        implements UserMessageReadService {
+
+}
+
+
+
+
