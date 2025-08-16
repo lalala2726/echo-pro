1740bf3bea73811321aac0ee7a3e1c3d5f8e1cf6
Chuang <Chuang003@outlook.com>
2025-08-16 14:39:06 +0800

    refactor(system): 优化角色相关模型的 Swagger 文档注释- 为 BaseVo、SysRoleListVo、SysRolePermissionVo 和 SysRoleVo 类的字段添加了更详细的 Swagger 注释 -增加了字段的 type、example 和 allowableValues 等属性，以提供更丰富的文档信息- 调整了部分字段的描述，使其更加准确和一致

--- chuang-system/chuang-system-core/src/main/java/cn/zhangchuangla/system/core/model/vo/role/SysRoleVo.java
+++ chuang-system/chuang-system-core/src/main/java/cn/zhangchuangla/system/core/model/vo/role/SysRoleVo.java
@@ -3,6 +3,7 @@ package cn.zhangchuangla.system.core.model.vo.role;
 import cn.zhangchuangla.common.core.entity.base.BaseVo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -28,19 +29,27 @@ public class SysRoleVo extends BaseVo {
     /**
      * 角色名
      */
-    @Schema(description = "角色名")
+    @Schema(description = "角色名", type = "string", example = "管理员")
     private String roleName;
+
+
+    /**
+     * 角色状态（0正常 1停用）
+     */
+    @Schema(description = "角色状态（0正常 1停用）", type = "string", allowableValues = {"0", "1"}, example = "0")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Integer status;
 
     /**
      * 角色权限标识
      */
-    @Schema(description = "角色权限标识")
+    @Schema(description = "角色权限标识", type = "string", example = "admin")
     private String roleKey;
 
     /**
      * 显示顺序
      */
-    @Schema(description = "显示顺序")
+    @Schema(description = "显示顺序", type = "string", example = "1")
     private Integer sort;
 
 
